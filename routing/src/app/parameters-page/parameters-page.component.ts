47cfcc5f0ba95d1e91933a70794b4d61d03f29ea
kelsonbatista <kelsonbatista@gmail.com>
2022-11-04 16:29:43 -0300

    feat: query parameters

--- routing/src/app/parameters-page/parameters-page.component.ts
+++ routing/src/app/parameters-page/parameters-page.component.ts
@@ -8,12 +8,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ParametersPageComponent implements OnInit {
   id: number | null = null;
+  nome: string | null = null;
+  idade: number | null = null;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.id = Number(params.get("id"));
+    })
+
+    this.route.queryParamMap.subscribe(params => {
+      this.nome = params.get("nome");
+      this.idade = Number(params.get("idade"));
     })
   }
 
