fec02e7cca7a08b7bb492a79157f7b74268bcbe1
kelsonbatista <kelsonbatista@gmail.com>
2022-11-06 18:31:47 -0300

    feat: added students table

---
+++ bootstrap/src/app/students/students.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { StudentService } from '../student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  constructor(
+    public studentService: StudentService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
