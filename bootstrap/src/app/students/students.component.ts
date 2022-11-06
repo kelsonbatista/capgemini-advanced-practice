088a1e23db42b8c389e4d7712c432fba56ed4a7c
kelsonbatista <kelsonbatista@gmail.com>
2022-11-06 19:56:39 -0300

    feat: added delete function

--- bootstrap/src/app/students/students.component.ts
+++ bootstrap/src/app/students/students.component.ts
@@ -8,12 +8,18 @@ import { StudentService } from '../student.service';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  students: Student[] = [];
 
   constructor(
     public studentService: StudentService
   ) { }
 
   ngOnInit(): void {
+    this.students = this.studentService.getAllStudents();
   }
 
+  deleteOneStudent(studentId: number) {
+    this.students = this.students.filter(student => student.id != studentId);
+    this.studentService.deleteOneStudent(studentId);
+  }
 }
