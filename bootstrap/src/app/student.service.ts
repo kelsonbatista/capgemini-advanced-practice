6147e0f8a72360e8e4ecf7db0462f6f437bf15be
kelsonbatista <kelsonbatista@gmail.com>
2022-11-11 22:49:34 -0300

    feat: student modules

--- bootstrap/src/app/student.service.ts
+++ bootstrap/src/app/student.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
-import { IStudent } from './student.interface';
+import { Student } from './student';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-  students: IStudent[] = [];
-  student: IStudent | undefined;
+  students: Student[] = [];
+  student: Student | undefined;
 
   constructor() { }
 
   getAllStudents() {
-    return JSON.parse(localStorage.getItem("students") || "[]");
+    this.students = JSON.parse(localStorage.getItem("students") || "[]");
+    return this.students
   }
 
   getOneStudent(studentId: number) {
-    return this.students.find(student => student.id == studentId);
+    this.getAllStudents();
+    this.student = this.students.find(student => student.id == studentId);
+    return this.student
   }
 
-  addStudent(student: IStudent) {
+  addStudent(student: Student) {
+    this.getAllStudents();
     this.students.push(student);
     localStorage.setItem("students", JSON.stringify(this.students));
   }
 
   deleteOneStudent(studentId: number) {
+    this.getAllStudents();
     this.students = this.students.filter(student => student.id != studentId);
     localStorage.setItem("students", JSON.stringify(this.students));
   }
