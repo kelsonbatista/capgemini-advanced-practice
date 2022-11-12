3f7299182debcf702deee3ed08bd0072fa95ea89
kelsonbatista <kelsonbatista@gmail.com>
2022-11-11 23:00:20 -0300

    feat: added index.js

---
+++ PWA-example/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// ejs para viualização
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('../view/index');
+});
+
+app.listen(3000, function() {
+  console.log('Executando na porta 3000');
+});
