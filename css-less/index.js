5c4c1201e0d05ece808c9a8333604d50d9e0f414
kelsonbatista <kelsonbatista@gmail.com>
2022-11-12 10:51:32 -0300

    feat: added basic less structure

---
+++ css-less/index.js
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet/less" type="text/css" href="style.less" />
+  <title>Teste</title>
+</head>
+<body>
+  <h1>Teste LESS</h1>
+
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem ab laudantium repellendus odit omnis sit, ex maiores eum aut iste facilis enim. Adipisci qui aliquid vero? Earum recusandae facere ab. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus placeat, quae ex quidem optio quod, molestias suscipit excepturi dolorum corrupti maiores deserunt natus ad rem enim itaque voluptas ipsum cupiditate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nemo doloribus perspiciatis ratione, deleniti vitae expedita magni quisquam explicabo, repudiandae tenetur aperiam neque quos quae qui, deserunt nulla in similique.</p>
+
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+    <li>Item 5</li>
+  </ul>
+
+  <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+  <script>
+    less = {
+      env = "development"  // or "production" - se for dev, ele vai mostrar os erros
+    };
+  </script>
+</body>
+</html>
